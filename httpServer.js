cf45ca15a7ae863ed6105a9b92152fb916b254cd
David Miller <hawky454@gmail.com>
2018-06-12 15:37:17 -0500

    Starting 2nd part of assignment

--- httpServer.js
+++ httpServer.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000;
 
 const server = http.createServer(function(req, res) {
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello world');
+  res.end('Hello world, fucking hell, I feel like a retardo.... now what?');
 });
 
 server.listen(port, function() {
