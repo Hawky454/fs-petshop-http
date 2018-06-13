4d273e8745d6ba4523902b2fa3e86dcf502c73a8
David Miller <hawky454@gmail.com>
2018-06-13 15:29:12 -0500

    Finished assignment excluding the bonus

--- expressServer.js
+++ expressServer.js
@@ -10,8 +10,9 @@ console.log("Express obj:", express);
 let app = express();
 let port = process.env.PORT || 8000;
 
-app.disable('x-powered-by');
+app.disable('x-powered-by');//hides that it's powered by Express.
 
+//http GET localhost:8000/pets
 app.get('/pets', function(req, res) {
   fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
     if (err) {
@@ -20,11 +21,12 @@ app.get('/pets', function(req, res) {
     }
 
     let pets = JSON.parse(petsJSON);
-
-    res.send(pets);
+    res.set('Content-Type', 'application/json')
+    res.status(200).send(pets);
   });
 });
 
+//http GET localhost:8000/pets/0
 app.get('/pets/:id', function(req, res) {
   fs.readFile(petsPath, 'utf8', function(err, petsJSON){
     if (err) {
@@ -39,13 +41,19 @@ app.get('/pets/:id', function(req, res) {
       return res.sendStatus(404);
     }
 
-    res.set('Content-Type', 'text/plain');
+    res.set('Content-Type', 'application/json');
     res.send(pets[id]);
   });
 });
 
+app.post('/pets', function (req, res) {
+
+})
+
 app.use(function(req, res) {
+  res.set('Content-Type', 'text/plain');
   res.sendStatus(404);
+  console.log(pets);
 });
 
 app.listen(port, function() {
